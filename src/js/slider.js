5f397155787388470f84fe81e16f3a260bfd8de1
Targonkp Targonkp <targon_k_p@mail.ru>
2022-08-08 16:08:17 +0300

    slider fixed (added setTimeout), added Readme

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,33 +4,34 @@ let sliderElements = document.querySelectorAll('.review');
 let dotsWrap = document.querySelector('.slider-dots');
 //получаю обертку слайдов
 let sliderWrap = document.querySelector('.reviews-wrap');
+//задаю переменную для списка точек
+let dotsList;
 //задаю начальный индекс для элементов слайдера
 let indexEl = Math.floor(sliderElements.length/2);
 //значение единицы margin-left
 let movingEl;
 
+
 //функция, отвечающая за размер movingEl для margin-left, в зависимости от размера экрана
 function sliderMoving(){
     if (document.documentElement.clientWidth > 768){
         movingEl = 540;
-        console.log(movingEl)
         sliderChange(sliderElements, indexEl);
     }
     if (document.documentElement.clientWidth <= 768 && document.documentElement.clientWidth > 480)
     {
         movingEl = 420;
-        console.log(movingEl)
         sliderChange(sliderElements, indexEl);
     }
     if (document.documentElement.clientWidth <= 480 ){
         movingEl = 300;
-        console.log(movingEl)
         sliderChange(sliderElements, indexEl);
     }
 }
 
 sliderMoving();
 
+//слежу за изменением размеров окна
 window.addEventListener(
     'resize',
     sliderMoving
@@ -39,14 +40,15 @@ window.addEventListener(
 
 //функция для создания точек слайдера
 function createDots(array, index) {
-    //выполняю проверку длины массива, чтбы не возникала ошибкас навешиванием активного класса
+    //выполняю проверку длины массива, чтбы не возникала ошибка с навешиванием активного класса
     if (array.length > 0){
         for (let i = 0; i< array.length; i++){
             let dotEl = document.createElement('span');
             dotEl.className = 'slider-dots__element';
             dotsWrap.appendChild(dotEl);
         }
-        document.querySelectorAll('.slider-dots__element')[index].classList.add('slider-dots__element_active');
+        dotsList = document.querySelectorAll('.slider-dots__element');
+        dotsList[index].classList.add('slider-dots__element_active');
     }
 }
 
@@ -63,7 +65,6 @@ function sliderChange(array, index){
     let changeMargin = (index - 1) * movingEl;
     //задаю обертке отрицательный margin, чтобы двигать слайдер
     sliderWrap.style.marginLeft = -changeMargin + 'px';
-    console.log(changeMargin);
 }
 
 //навешиваю обработчик на общего родителя точек
@@ -80,9 +81,36 @@ dotsWrap.addEventListener(
             event.target.classList.add('slider-dots__element_active')
         }
         indexEl = dotsList.findIndex(index => index.classList.contains('slider-dots__element_active'));
-        console.log(indexEl);
         sliderChange(sliderElements, indexEl);
     }
+)
+
+setTimeout(
+    function sliderRun() {
+        if (indexEl < sliderElements.length-1) {
+            indexEl++;
+        }
+        else
+        {
+            indexEl = 0;
+        }
+
+        //удаляю у всех точек активный класс
+        dotsList.forEach(
+            item => {
+                item.classList.remove('slider-dots__element_active')
+            }
+        )
+
+        //задаю активный класс точке с текущим index
+        dotsList[indexEl].classList.add('slider-dots__element_active')
+
+        //вызываю функцию изменения слайдера
+        sliderChange(sliderElements, indexEl);
+        setTimeout(
+            sliderRun, 5000
+        )
+    }, 5000
 )
 
 
